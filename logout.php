ac1a6eed1c12dec7f8e726de5ef0bf02d56f7071
DisconnectedHLV <79093918+DisconnectedHLV@users.noreply.github.com>
2021-02-15 20:59:49 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-
-// mengahancurkan $_SESSION["pelanggan"]
-session_destroy();
-echo "<script>alert('Anda Telah Logout');</script>";
-echo "<script>location='utama.php';</script>";
-
- ?>
